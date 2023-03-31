513e4348e2678c5ed55096fe53ed67f0b71cc8af
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-03-31 20:57:20 +0600

    lesson 9 done

--- src/Views/ContactView.vue
+++ src/Views/ContactView.vue
@@ -1,5 +1,16 @@
-<script src=""></script>
+<script setup>
+import cars from "../data/cars.json";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const car = cars.find((c) => c.id == parseInt(route.params.id));
+</script>
 
 <template>
-  <div>contact view</div>
+  <div>
+    <h1>Contact info</h1>
+    <p>{{ car.contact.dealer }}</p>
+    <p>{{ car.contact.number }}</p>
+  </div>
 </template>
